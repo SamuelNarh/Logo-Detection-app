d30dc389b230604752fe49dbffab1fe30d70902c
samuel narh <samuelnarh38@gmail.com>
2023-05-24 13:07:24 +0000

    Add files via upload
    configured api and was able to get the bounding box position console logged

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
   constructor(){
     super()
    this.state={
-    input:"",
+    input:'',
     imageUrl:''
    }
   }
@@ -17,8 +17,52 @@ class App extends Component{
   }
   onSubmit =()=>{
     this.setState({imageUrl:this.state.input})
-    console.log(this.state.imageUrl)
+    
+const raw = JSON.stringify({
+  "user_app_id": {
+    "user_id": "clarifai",
+    "app_id": "main"
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": this.state.imageUrl
+              }
+          }
+      }
+  ]
+});
+
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + 'b0deb4f279bd4e1faef4051a6081b314'
+    },
+    body: raw
+};
+
+
+fetch(`https://api.clarifai.com/v2/models/logos-yolov5/versions/d4e85f4d37d24fdd9a1f04cca91a3510/outputs`, requestOptions)
+    .then(response => response.json())
+    .then(result => this.onResult(result))
+    .catch(error => console.log('error', error));
+  
   }
+
+  onResult =(input)=>{
+    let result=input.outputs[0].data.regions;
+    // console.log('input',result.length,result)
+   result.forEach(element => {
+    console.log(element.region_info.bounding_box)
+   });
+  }
+
+  bounding_box =()=>{
+    
+  }
+  
   render()
   {
     return(
